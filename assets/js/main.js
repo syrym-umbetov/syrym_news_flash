91f16f87eb99e8c2f6d9c98d95807ede4f196faf
syrym-umbetov <umbetov.syrym@gmail.com>
2022-03-05 20:02:36 +0600

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+// Grab elements
+const selectElement = (selector) => {
+  const element = document.querySelector(selector);
+  if (element) return element;
+  throw new Error(`Something went wrong, make sure that &(selector)`);
+};
+//Nav styles on scroll
+
+// Open menu & search pop-up
+
+// Open/Close search form popup
+
+// -- Close the search form popup on ESC keypress
+
+// Switch theme/add to local storage
+
+// Swiper
